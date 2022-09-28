500c5e01b78a24d8eb7edd94661d53648e7e07fb
RDG97 <ryangann1997@gmail.com>
2022-09-28 19:04:37 +0000

    modified:   index.html modified:   js/main.js modified:   pseudocode.html

--- js/main.js
+++ js/main.js
@@ -13,10 +13,10 @@ const main = document.getElementById('main');
 const zipEnter = document.getElementById('zipEnter');
 const ace = document.getElementById('ace');
 const card = document.getElementById('cardBody');
-const cityContainer = document.getElementById('cityContainer');
+const cityCont = document.getElementById('cityCont');
 const cityTop = document.getElementById('cityTop')
 const cityName = document.getElementById('cityName');
-const tempContainer = document.getElementById('tempContainer');
+const tempCont = document.getElementById('tempCont');
 const tempTop = document.getElementById('tempTop');
 const tempSpan = document.getElementById('tempSpan');
 const temp1 = document.getElementById('temp1');
@@ -25,21 +25,21 @@ const temp3 = document.getElementById('temp3');
 const conditionContainer = document.getElementById('conditionContainer');
 const conditionTop = document.getElementById('conditionTop');
 const theCondition = document.getElementById('theCondition');
-const otherContainer = document.getElementById('otherContainer');
+const otherCont = document.getElementById('otherCont');
 const otherTop = document.getElementById('otherTop');
 const other = document.getElementById('other');
+createZip();
+function init() {
+  createCard();
+  createCity();
+  createTemps();
+  createCond();
+  createOther();
+  
+};
 
 
 
-
-  function addEl() {
-    const main = document.getElementById('main')
-    const newDiv = document.createElement("div");
-    const newContent = document.createTextNode("Hi there and greetings!");
-    newDiv.appendChild(newContent);
-    const currentDiv = document.getElementById("div1");
-    main.insertAdjacentElement('beforeend', newDiv);
-  };
 
 
 function createZip() { 
@@ -49,8 +49,8 @@ function createZip() {
     zipBtn.setAttribute('type','submit') // same thing as above line but sets zipBtn to be an actual btn
     zipBtn.setAttribute('value','submit zip code') //setting zipBtn value... see the trend?
     zipBtn.setAttribute('id','zipBtn')
-    main.insertAdjacentElement('beforeend', zipEnter); //very helpful js command to choose where you want element to go
-    main.insertAdjacentElement('beforeend', zipBtn)  //main is the ID of the one div hardcoded to HTML
+    main.insertAdjacentElement('afterbegin', zipEnter); //very helpful js command to choose where you want element to go
+    main.insertAdjacentElement('afterbegin', zipBtn)  //main is the ID of the one div hardcoded to HTML
 };
 
 function createCard() {
@@ -60,7 +60,145 @@ function createCard() {
   const makeCardBody = document.createElement('div');
   makeCardBody.setAttribute('id','cardBody');
   makeCardBody.setAttribute('class','card-body');
-  main.insertAdjacentElement('afterbegin', makeCard);
+  const makeCardHeader = document.createElement('div');
+  makeCardHeader.setAttribute('id','zipHere')
+  makeCardHeader.setAttribute('class','card-header');
+  main.insertAdjacentElement('beforeend', makeCard);
   const ace = document.getElementById('ace');
   ace.insertAdjacentElement('afterbegin', makeCardBody);
+  ace.insertAdjacentElement('afterbegin', makeCardHeader);
 };
+
+function createCity() {
+  const makeCityCont = document.createElement('div');
+  makeCityCont.setAttribute('id','cityCont')
+  makeCityCont.setAttribute('class','container p-2 my-3 border text-center');
+  const makeCityTop = document.createElement('div');
+  makeCityTop.setAttribute('class','row bg-warning text-center');
+  makeCityTop.setAttribute('id','cityTop')
+  const makeCityName = document.createElement('div');
+  makeCityName.setAttribute('id','cityName')
+  makeCityName.setAttribute('class','display-6')
+  const makecName = document.createElement('p')
+  const cityTitle = document.createElement('p')
+  cityTitle.setAttribute('id','cityTitle')
+  makecName.setAttribute('id','cName')
+  const cName = document.getElementById('cName')
+  cardBody.insertAdjacentElement('afterbegin', makeCityCont)
+  const cityCont = document.getElementById('cityCont')
+  cityCont.insertAdjacentElement('afterbegin', makeCityTop)
+  cityCont.insertAdjacentElement('beforeend', makeCityName);
+  const cityName = document.getElementById('cityName')
+  cityName.insertAdjacentElement('afterbegin', makecName);
+  document.getElementById('cName').textContent = ':Lexington:';
+  const cityTop = document.getElementById('cityTop')
+  cityTop.insertAdjacentElement('afterbegin', cityTitle)
+  document.getElementById('cityTitle').textContent = 'City'
+};
+
+function createTemps() {
+  const makeTempCont = document.createElement('div')
+  makeTempCont.setAttribute('id','tempCont');
+  makeTempCont.setAttribute('class','container p-2 my-3 border text-center');
+  const makeTempTop = document.createElement('div');
+  makeTempTop.setAttribute('id','tempTop')
+  makeTempTop.setAttribute('class','row bg-warning')
+  const makeTempTitle = document.createElement('h3')
+  makeTempTitle.setAttribute('id','tempTitle')
+  const makeTempSpan = document.createElement('div');
+  makeTempSpan.setAttribute('id','tempSpan');
+  makeTempSpan.setAttribute('class','d-inline-flex display-6');
+  const makeTemp1 = document.createElement('div');
+  makeTemp1.setAttribute('id','temp1')
+  makeTemp1.setAttribute('class','p-2 border')
+  const makeTempK = document.createElement('p');
+  makeTempK.setAttribute('id','tempK');
+  const makeTemp2 = document.createElement('div')
+  makeTemp2.setAttribute('id','temp2')
+  makeTemp2.setAttribute('class','p-2 border')
+  const makeTempF = document.createElement('p');
+  makeTempF.setAttribute('id','tempF')
+  const makeTemp3 = document.createElement('div')
+  makeTemp3.setAttribute('id','temp3')
+  makeTemp3.setAttribute('class','p-2 border');
+  const makeTempC = document.createElement('p');
+  makeTempC.setAttribute('id','tempC')
+  cardBody.insertAdjacentElement('beforeend', makeTempCont);
+  const tempCont = document.getElementById('tempCont');
+  tempCont.insertAdjacentElement('afterbegin', makeTempTop);
+  const tempTop = document.getElementById('tempTop');
+  tempTop.insertAdjacentElement('afterbegin', makeTempTitle);
+  document.getElementById('tempTitle').textContent = 'Temperature';
+    tempCont.insertAdjacentElement('beforeend', makeTempSpan)
+  const tempSpan = document.getElementById('tempSpan');
+  tempSpan.insertAdjacentElement("afterbegin", makeTemp1)
+  tempSpan.insertAdjacentElement("afterbegin", makeTemp2)
+  tempSpan.insertAdjacentElement("afterbegin", makeTemp3)
+  const temp1 = document.getElementById('temp1');
+  const temp2 = document.getElementById('temp2');
+  const temp3 = document.getElementById('temp3');
+  temp1.insertAdjacentElement('afterbegin', makeTempK)
+  temp2.insertAdjacentElement('afterbegin', makeTempF)
+  temp3.insertAdjacentElement('afterbegin', makeTempC)
+  const tempK = document.getElementById('tempK');
+  const tempF = document.getElementById('tempF');
+  const tempC = document.getElementById('tempC');
+  document.getElementById('tempK').textContent = ':K:';
+  document.getElementById('tempF').textContent = ':F:';
+  document.getElementById('tempC').textContent = ':C:';
+}
+
+function createCond() {
+  const makeConditionCont = document.createElement('div');
+  makeConditionCont.setAttribute('id','conditionCont');
+  makeConditionCont.setAttribute('class','container p-2 my-3 border text-center');
+  const makeConditionTop = document.createElement('div');
+  makeConditionTop.setAttribute('id','conditionTop');
+  makeConditionTop.setAttribute('class',"row bg-warning");
+  const makeConditionTitle = document.createElement('h3')
+  makeConditionTitle.setAttribute('id','conditionTitle')
+  const makeTheCondition = document.createElement('div')
+  makeTheCondition.setAttribute('id','theCondition')
+  makeTheCondition.setAttribute('class','display-6')
+  const makeCond = document.createElement('p');
+  makeCond.setAttribute('id','cond')
+  cardBody.insertAdjacentElement('beforeend', makeConditionCont);
+  const conditionCont = document.getElementById('conditionCont');
+  conditionCont.insertAdjacentElement('afterbegin', makeConditionTop);
+  const conditionTop = document.getElementById('conditionTop');
+  conditionTop.insertAdjacentElement('afterbegin', makeConditionTitle);
+  const conditionTitle = document.getElementById('conditionTitle')
+  document.getElementById('conditionTitle').textContent = 'Condition';
+  conditionCont.insertAdjacentElement('beforeend', makeTheCondition);
+  const theCondition = document.getElementById('theCondition')
+  theCondition.insertAdjacentElement('afterbegin', makeCond);
+  const cond = document.getElementById('cond');
+  document.getElementById('cond').textContent = ':not great:';
+}
+
+function createOther() {
+  const createOtherCont = document.createElement('div')
+  createOtherCont.setAttribute('id','otherCont')
+  createOtherCont.setAttribute('class','container p-2 my-3 border text-center');
+  const createOtherTop = document.createElement('div');
+  createOtherTop.setAttribute('id','otherTop');
+  createOtherTop.setAttribute('class','row bg-warning');
+  const createOtherTitle = document.createElement('h3');
+  createOtherTitle.setAttribute('id','otherTitle');
+  const createOtherImg = document.createElement('p')
+  createOtherImg.setAttribute('id','otherImg');
+  createOtherImg.setAttribute('class','display-6');
+  cardBody.insertAdjacentElement('beforeend', createOtherCont);
+  const otherCont = document.getElementById('otherCont');
+  otherCont.insertAdjacentElement('afterbegin', createOtherTop)
+  const otherTop = document.getElementById('otherTop')
+  otherTop.insertAdjacentElement('afterbegin', createOtherTitle);
+  const otherTitle = document.getElementById('otherTitle');
+  document.getElementById('otherTitle').textContent = 'Other';
+  otherCont.insertAdjacentElement('beforeend', createOtherImg)
+  const otherImg = document.getElementById('otherImg')
+  document.getElementById('otherImg').textContent = 'a sweet image will go here. im just trying to take up space for appropriate box size';
+
+
+
+}
